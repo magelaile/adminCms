b2f253b1e8839a4660be30fda46dfaba74b64491
magelaile <707909622@qq.com>
2021-02-09 11:24:57 +0800

    提交

--- config/app.php
+++ config/app.php
@@ -10,8 +10,6 @@ return [
     'app_namespace'    => '',
     // 是否启用路由
     'with_route'       => true,
-    //开启应用快速访问
-    'app_express'    =>    true,
     // 默认应用
     'default_app'      => 'home',
     // 默认时区
@@ -22,7 +20,7 @@ return [
     // 域名绑定（自动多应用模式有效）
     'domain_bind'      => [],
     // 禁止URL访问的应用列表（自动多应用模式有效）
-    'deny_app_list'    => [],
+    'deny_app_list'    => ['admin'],
 
     // 异常页面的模板文件
     'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
